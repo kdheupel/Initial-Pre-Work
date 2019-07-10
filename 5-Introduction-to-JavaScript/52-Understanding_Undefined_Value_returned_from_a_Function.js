f347118a6d15e7d38b3b90280f897bd20f237df7
kdheupel <kdheupel@gmail.com>
2019-07-10 17:44:37 -0600

    created a addition function with undefined elements

---
+++ 5-Introduction-to-JavaScript/52-Understanding_Undefined_Value_returned_from_a_Function.js
@@ -0,0 +1,16 @@
+created a addition function with undefined elements
+
+// Example
+var sum = 0;
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive() {
+  sum = sum + 5;
+}
+
+
+// Only change code above this line
+var returnedValue = addFive();
