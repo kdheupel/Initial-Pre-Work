4e222e827acce7bf758e096bd3f1fbe21e5de28b
kdheupel <kdheupel@gmail.com>
2019-07-10 14:40:29 -0600

    counting to the third variable by using 2 since the first variable is 0

---
+++ 5-Introduction-to-JavaScript/32-Use_Bracket_Notation_to_Find_the_Nth_Character_in_a_String.js
@@ -0,0 +1,11 @@
+counting to the third variable by using 2 since the first variable is 0
+
+// Example
+var firstName = "Ada";
+var secondLetterOfFirstName = firstName[1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var thirdLetterOfLastName = lastName[2];
