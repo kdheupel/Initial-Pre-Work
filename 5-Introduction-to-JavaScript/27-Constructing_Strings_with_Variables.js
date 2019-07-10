c1cabdb7ffe189a356ed462476442902368a0e70
kdheupel <kdheupel@gmail.com>
2019-07-10 13:17:08 -0600

    construcing a sentence by having a variable term in the middle; such as my name

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,9 @@
+construcing a sentence by having a variable term in the middle; such as my name
+
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "KDH";
+var myStr = "My name is " + myName + " and I am well!";
