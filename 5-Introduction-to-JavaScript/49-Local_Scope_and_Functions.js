faaad129c7928b2fa3b85289396e709f861b8199
kdheupel <kdheupel@gmail.com>
2019-07-10 16:51:55 -0600

    creating a local - non global - variable using name myVar

---
+++ 5-Introduction-to-JavaScript/49-Local_Scope_and_Functions.js
@@ -0,0 +1,13 @@
+creating a local - non global - variable using name myVar
+
+function myLocalScope() {
+  'use strict'; // you shouldn't need to edit this line
+  var myVar;
+  console.log(myVar);
+}
+myLocalScope();
+
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+
+// Now remove the console log line to pass the test
