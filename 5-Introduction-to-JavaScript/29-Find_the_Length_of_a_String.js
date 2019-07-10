59d66bd0e796bcad6f22f828e8cd493d495aeb90
kdheupel <kdheupel@gmail.com>
2019-07-10 13:21:30 -0600

    determining the length of a variable output; such as last name

---
+++ 5-Introduction-to-JavaScript/29-Find_the_Length_of_a_String.js
@@ -0,0 +1,15 @@
+determining the length of a variable output; such as last name
+
+// Example
+var firstNameLength = 0;
+var firstName = "Ada";
+
+firstNameLength = firstName.length;
+
+// Setup
+var lastNameLength = 0;
+var lastName = "Lovelace";
+
+// Only change code below this line.
+
+lastNameLength = lastName.length;
