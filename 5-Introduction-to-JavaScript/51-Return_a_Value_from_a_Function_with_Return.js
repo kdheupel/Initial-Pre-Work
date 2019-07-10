d93fa5f3401f99e63097b3c9ae2c000b3d88e583
kdheupel <kdheupel@gmail.com>
2019-07-10 17:02:52 -0600

    using a num argument for multiplication work

---
+++ 5-Introduction-to-JavaScript/51-Return_a_Value_from_a_Function_with_Return.js
@@ -0,0 +1,16 @@
+using a num argument for multiplication work
+
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+function timesFive(num) {
+  return num * 5;
+}
+var answer = timesFive(5);
+var answer = timesFive(2);
+var answer = timesFive(0);
+
+console.log(timesFive(5));
