26f3684924a90d15b7c35479ab6b88a845836396
kdheupel <kdheupel@gmail.com>
2019-07-10 12:47:54 -0600

    Using double quotes for when you have a quote within sentence.  When I went to the next assignment, I realized my error so I corrected it and resent.  I put my code corrections on the wrong line initially.

--- 5-Introduction-to-JavaScript/22-Escaping-Literal-Quotes-in-Strings.js
+++ 5-Introduction-to-JavaScript/22-Escaping-Literal-Quotes-in-Strings.js
@@ -1,5 +1,3 @@
 Using double quotes for when you have a quote within sentence
 
-var myStr = ""; // Change this line
-
-var myStr = "I am a \"double quoted\" string inside \"double quotes\".";
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
