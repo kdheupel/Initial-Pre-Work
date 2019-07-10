5c3685d18097b9441322d78b15fd71ac2a2a8792
kdheupel <kdheupel@gmail.com>
2019-07-10 16:35:55 -0600

    created a function with arguments to perform two different addition calculations

---
+++ 5-Introduction-to-JavaScript/47-Passing_Values_to_Functions_with_Arguments.js
@@ -0,0 +1,15 @@
+created a function with arguments to perform two different addition calculations
+
+// Example
+function ourFunctionWithArgs(a, b) {
+  console.log(a - b);
+}
+ourFunctionWithArgs(10, 5); // Outputs 5
+
+// Only change code below this line.
+function functionWithArgs(a, b) {
+  console.log(a + b);
+}
+
+functionWithArgs(1,2);
+functionWithArgs(7,9);
