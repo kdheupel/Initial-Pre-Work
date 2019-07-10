2bb850ea8c08cf57e769adbe7a15fb18189be2c8
kdheupel <kdheupel@gmail.com>
2019-07-10 13:18:53 -0600

    Using the += for variable terms in a sentence

---
+++ 5-Introduction-to-JavaScript/28-Appending_Variables_to_Strings.js
@@ -0,0 +1,12 @@
+Using the += for variable terms in a sentence
+
+// Example
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+
+// Only change code below this line
+
+var someAdjective = "fun";
+var myStr = "Learning to code is ";
+myStr += someAdjective;
