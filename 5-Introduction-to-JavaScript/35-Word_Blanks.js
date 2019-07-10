586203b8173cc80d464aabcefb7d6abfdf4d638b
kdheupel <kdheupel@gmail.com>
2019-07-10 15:07:56 -0600

    Did a word blank example - pretty challenging

---
+++ 5-Introduction-to-JavaScript/35-Word_Blanks.js
@@ -0,0 +1,13 @@
+Did a word blank example
+
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  // Your code below this line
+   var result = "My "+myAdjective+" "+myNoun+" "+myVerb+" "+myAdverb+".";
+
+  // Your code above this line
+  return result;
+}
+
+// Change the words here to test your function
+wordBlanks("dog", "big", "ran", "quickly");
+wordBlanks("cat", "little", "hit", "slowly");
