2608747fb5833a7909dd7cfc76139ec451e96535
kdheupel <kdheupel@gmail.com>
2019-07-10 14:37:17 -0600

    using a bracket to determine variable declarations

---
+++ 5-Introduction-to-JavaScript/30-Use_Bracket_Notation_to_Find_the_First_Character_in_a_String.js
@@ -0,0 +1,14 @@
+using a bracket to determine variable declarations
+
+// Example
+var firstLetterOfFirstName = "";
+var firstName = "Ada";
+
+firstLetterOfFirstName = firstName[0];
+
+// Setup
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0];
