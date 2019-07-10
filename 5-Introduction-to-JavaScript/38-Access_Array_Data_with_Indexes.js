1667e4a2870d2073afe0398df1e5118d1032e20f
kdheupel <kdheupel@gmail.com>
2019-07-10 15:45:56 -0600

    accessing array data with indexes by using brackets

---
+++ 5-Introduction-to-JavaScript/38-Access_Array_Data_with_Indexes.js
@@ -0,0 +1,11 @@
+accessing array data with indexes by using brackets
+
+// Example
+var ourArray = [50,60,70];
+var ourData = ourArray[0]; // equals 50
+
+// Setup
+var myArray = [50,60,70];
+
+// Only change code below this line.
+var myData = myArray[0];
