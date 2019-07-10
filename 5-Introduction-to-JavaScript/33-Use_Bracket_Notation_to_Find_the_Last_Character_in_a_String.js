841824e1213d9ba167d703d8081846ee397a9b3e
kdheupel <kdheupel@gmail.com>
2019-07-10 14:46:29 -0600

    determining the last letter of name using brackets - had some issues b/c i kept typing lastname instead of lastName

---
+++ 5-Introduction-to-JavaScript/33-Use_Bracket_Notation_to_Find_the_Last_Character_in_a_String.js
@@ -0,0 +1,11 @@
+determining the last letter of name using brackets 
+
+// Example
+var firstName = "Ada";
+var lastLetterOfFirstName = firstName[firstName.length - 1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var lastLetterOfLastName = lastName[lastName.length - 1];
