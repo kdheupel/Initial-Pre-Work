df37cb4fe205c07562f64387b6880039a4eb1ca0
kdheupel <kdheupel@gmail.com>
2019-07-10 12:31:22 -0600

    Learned a string variable using my first and last name

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,9 @@
+Learned a string variable using my first and last name 
+
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+var myFirstName = "Kevin";
+var myLastName = "Heupel";
