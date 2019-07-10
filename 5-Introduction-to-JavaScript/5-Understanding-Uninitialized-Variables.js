a9fabf2031fd0f2f8e6a801a144f660e0de78fb9
kdheupel <kdheupel@gmail.com>
2019-07-10 11:49:38 -0600

    avoiding an undefined script by defining each element

---
+++ 5-Introduction-to-JavaScript/5-Understanding-Uninitialized-Variables.js
@@ -0,0 +1,15 @@
+avoiding an undefined script by defining each element
+
+// Initialize these three variables
+var a;
+var b;
+var c;
+
+// Do not change code below this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+a=6;
+b=15;
+c="I am a String!"
