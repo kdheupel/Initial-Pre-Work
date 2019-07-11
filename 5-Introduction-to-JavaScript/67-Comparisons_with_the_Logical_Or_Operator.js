350a16b4c39b0f41141c49b68d578c5ef0750c96
kdheupel <kdheupel@gmail.com>
2019-07-10 19:41:25 -0600

    created code for numbers outside of a range using ||

---
+++ 5-Introduction-to-JavaScript/67-Comparisons_with_the_Logical_Or_Operator.js
@@ -0,0 +1,22 @@
+created code for numbers outside of a range using ||
+
+function testLogicalOr(val) {
+  // Only change code below this line
+
+  if (val > 20 || val < 10) {
+    return "Outside";
+  }
+
+  // Only change code above this line
+  return "Inside";
+}
+
+// Change this value to test
+testLogicalOr(0);
+testLogicalOr(9);
+testLogicalOr(10);
+testLogicalOr(15);
+testLogicalOr(19);
+testLogicalOr(20);
+testLogicalOr(21);
+testLogicalOr(25);
