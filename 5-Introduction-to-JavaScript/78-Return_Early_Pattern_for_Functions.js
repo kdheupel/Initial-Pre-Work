4769823616d9d6554d42c85c970be5e2e7ece3a4
kdheupel <kdheupel@gmail.com>
2019-07-10 22:19:52 -0600

    Had to create an undefined result for a & b < 0.  I had to look on the Internet for a solution as I could not figure this one out on my own.

---
+++ 5-Introduction-to-JavaScript/78-Return_Early_Pattern_for_Functions.js
@@ -0,0 +1,20 @@
+had to create an undefined result for a & b < 0
+
+// Setup
+function abTest(a, b) {
+  // Only change code below this line
+if (a<0||b<0){
+  return undefined;
+  }
+
+  // Only change code above this line
+
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+// Change values below to test your code
+abTest(2,2);
+abTest(-2,2);
+abTest(2,-2);
+abTest(2,8);
+abTest(3,3);
