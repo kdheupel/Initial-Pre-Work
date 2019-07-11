0294169e5bd9212d6f0c0f4ca6e5700f96902ef7
kdheupel <kdheupel@gmail.com>
2019-07-10 21:46:11 -0600

    Used switch statements to set classes - had some difficulty with the :.  Kept using the ;

---
+++ 5-Introduction-to-JavaScript/73-Selecting_from_Many_Options_with_Switch_Statements.js
@@ -0,0 +1,30 @@
+Used switch statements to set classes - had some difficulty with the :.  Kept using the ;
+
+function caseInSwitch(val) {
+  var answer = "";
+  // Only change code below this line
+  switch(val) {
+    case 1:
+    return "alpha";
+    break;
+    case 2:
+    return "beta";
+    break;
+    case 3:
+    return "gamma"
+    break;
+    case 4:
+    return "delta";
+    break;
+  }
+
+
+  // Only change code above this line
+  return answer;
+}
+
+// Change this value to test
+caseInSwitch(1);
+caseInSwitch(2);
+caseInSwitch(3);
+caseInSwitch(4);
