068de0eea16495d702a5f92ea35c0082ac15a9ed
kdheupel <kdheupel@gmail.com>
2019-07-10 19:38:34 -0600

    using both <= and >= to determine a range

---
+++ 5-Introduction-to-JavaScript/66-Comparisons_with_the_Logical_And_Operator.js
@@ -0,0 +1,22 @@
+using both <= and >= to determine a range
+
+function testLogicalAnd(val) {
+  // Only change code below this line
+
+  if (val>=25 && val<=50) {
+          return "Yes";
+   }
+
+  // Only change code above this line
+  return "No";
+}
+
+// Change this value to test
+testLogicalAnd(0);
+testLogicalAnd(24);
+testLogicalAnd(25);
+testLogicalAnd(30);
+testLogicalAnd(50);
+testLogicalAnd(51);
+testLogicalAnd(75);
+testLogicalAnd(80);
