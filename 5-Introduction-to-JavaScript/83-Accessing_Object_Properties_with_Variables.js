5cdbc524a0bba9ce13968b46531e4ea5cb80af53
kdheupel <kdheupel@gmail.com>
2019-07-11 09:35:10 -0600

    accessed the objects properties with variables to match a jersey number with a player

---
+++ 5-Introduction-to-JavaScript/83-Accessing_Object_Properties_with_Variables.js
@@ -0,0 +1,13 @@
+accessed the objects properties with variables to match a jersey number with a player
+
+// Setup
+var testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line;
+
+var playerNumber = 16;       // Change this Line
+var player = testObj[playerNumber];   // Change this Line
