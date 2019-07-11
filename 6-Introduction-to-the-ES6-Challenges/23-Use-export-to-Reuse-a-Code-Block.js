b8e036d5e21c85e3f3291faa7453324ca295eecb
kdheupel <kdheupel@gmail.com>
2019-07-11 16:03:36 -0600

    exporting a portion of code for later use: a little tricky becase i didn't delte the first two lines before entering my expoert code

---
+++ 6-Introduction-to-the-ES6-Challenges/23-Use-export-to-Reuse-a-Code-Block.js
@@ -0,0 +1,5 @@
+exporting a portion of code for later use: a little tricky becase i didn't delte the first two lines before entering my expoert code
+
+"use strict";
+export const foo = "bar";
+export const bar = "foo";
