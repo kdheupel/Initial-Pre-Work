676a33e4b780bedbb76abc6a6e9e3e0952581342
kdheupel <kdheupel@gmail.com>
2019-07-10 21:28:00 -0600

    Learned multimple if/then statements

---
+++ 5-Introduction-to-JavaScript/71-Chaining_If_Else_Statements.js
@@ -0,0 +1,29 @@
+Learned multimple if/then stateements
+
+function testSize(num) {
+  // Only change code below this line
+  if (num < 5) {
+    return "Tiny";
+  } else if (num < 10) {
+    return "Small";
+  } else if (num < 15) {
+    return "Medium";
+  } else if (num < 20) {
+    return "Large"
+  } else {
+    return "Huge"
+  }
+}
+    // Only change code above this line
+
+// Change this value to test
+testSize(0);
+testSize(4);
+testSize(5);
+testSize(8);
+testSize(10);
+testSize(14);
+testSize(15);
+testSize(17);
+testSize(20);
+testSize(25);
