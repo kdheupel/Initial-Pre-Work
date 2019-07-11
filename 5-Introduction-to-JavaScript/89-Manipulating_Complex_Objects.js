a6c3285174dc6cd78ad7364cd8bc33dc6920b9ca
kdheupel <kdheupel@gmail.com>
2019-07-11 10:32:13 -0600

    added a second album.  the comma threw me off

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,28 @@
+added a second album
+
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }
+  // Add record here
+,
+  {
+    "artist": "Duran Duran",
+    "title": "Rio",
+    "release_year": 1983,
+    "formats": [
+      "CD",
+      "Music Video",
+      "LP"
+    ],
+    "gold": true
+    }
+];
