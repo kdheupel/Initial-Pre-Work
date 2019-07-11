ea240fc8ea037617fccd988c416958d0095b3617
kdheupel <kdheupel@gmail.com>
2019-07-10 18:40:47 -0600

    comparing different values when there is a number or a stringify

---
+++ 5-Introduction-to-JavaScript/59-Practice_comparing_different_values.js
@@ -0,0 +1,13 @@
+comparing different values when there is a number or a stringify
+
+// Setup
+function compareEquality(a, b) {
+  if (a === b) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+compareEquality(10, "10");
+compareEquality("20", 20);
