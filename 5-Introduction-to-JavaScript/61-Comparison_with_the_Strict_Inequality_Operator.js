0c13df20a4cd68435b97de350ba4211aa0f370db
kdheupel <kdheupel@gmail.com>
2019-07-10 19:28:08 -0600

    same as last exercise, but using a strict inequality of !==

---
+++ 5-Introduction-to-JavaScript/61-Comparison_with_the_Strict_Inequality_Operator.js
@@ -0,0 +1,20 @@
+same as last exercise, but using a strict inequality of !==
+
+// Setup
+function testStrictNotEqual(val) {
+  // Only Change Code Below this Line
+
+  if (val !==17) {
+
+  // Only Change Code Above this Line
+
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testStrictNotEqual(17);
+testStrictNotEqual("17");
+testStrictNotEqual(12);
+testStrictNotEqual("bob");
