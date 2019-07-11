c4799ad1f6d9d36f0c977c56ed62dac7ae4e742d
kdheupel <kdheupel@gmail.com>
2019-07-11 13:18:28 -0600

    Added the radix function along witht he pareInt to generate numbers

---
+++ 5-Introduction-to-JavaScript/105-Use_the_parseInt_Function_with_a_Radix.js
@@ -0,0 +1,11 @@
+Added the radix function along witht he pareInt to generate numbers
+
+function convertToInteger(str, radix) {
+  var radix = 2
+  return parseInt(str, radix);
+}
+
+convertToInteger("10011");
+convertToInteger("10011");
+convertToInteger("111001");
+convertToInteger("JamesBond");
