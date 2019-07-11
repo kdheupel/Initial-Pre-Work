3ea0f33b2a2a6a0a6b21192c46c7d194a7d8de3c
kdheupel <kdheupel@gmail.com>
2019-07-11 13:37:39 -0600

    Using let instead of var to avoid overwriting var variable later in the code

---
+++ 6-Introduction-to-the-ES6-Challenges/1-Explore-Differences-Between-the-var-and-let-Keywords.js
@@ -0,0 +1,12 @@
+Using let instead of var to avoid overwriting var variable later in the code
+
+let catName;
+let quote;
+function catTalk() {
+  "use strict";
+
+  catName = "Oliver";
+  quote = catName + " says Meow!";
+
+}
+catTalk();
