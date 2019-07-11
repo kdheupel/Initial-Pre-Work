32f9ab5e4fbe33b281ec3065cb19c95f0193ac96
kdheupel <kdheupel@gmail.com>
2019-07-10 21:55:27 -0600

    multiple switches within a range of numbers along with a descriptor

--- 5-Introduction-to-JavaScript/75-Multiple_Identical_Options_in_Switch_Statements.js
+++ 5-Introduction-to-JavaScript/75-Multiple_Identical_Options_in_Switch_Statements.js
@@ -1,1 +1,38 @@
+multiple switches within a range of numbers along with a descriptor
 
+function sequentialSizes(val) {
+  var answer = "";
+  // Only change code below this line
+  switch(val) {
+    case 1:
+    case 2:
+    case 3:
+    return "Low";
+    break;
+    case 4:
+    case 5:
+    case 6:
+    return "Mid";
+    break;
+    case 7:
+    case 8:
+    case 9:
+    return "High";
+    break;
+  }
+
+
+  // Only change code above this line
+  return answer;
+}
+
+// Change this value to test
+sequentialSizes(1);
+sequentialSizes(2);
+sequentialSizes(3);
+sequentialSizes(4);
+sequentialSizes(5);
+sequentialSizes(6);
+sequentialSizes(7);
+sequentialSizes(8);
+sequentialSizes(9);
