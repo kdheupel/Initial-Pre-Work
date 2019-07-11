e542db8359e1544a6edf33dfad78c114110aa500
kdheupel <kdheupel@gmail.com>
2019-07-11 12:55:27 -0600

    Learned to generate randon numbers

---
+++ 5-Introduction-to-JavaScript/101-Generate_Random_Fractions_with_JavaScript.js
@@ -0,0 +1,12 @@
+Learned to generate randon numbers
+
+function randomFraction() {
+
+  // Only change code below this line.
+var result = 0;
+    while (result === 0) {
+    result = Math.random();
+  }
+  return result;
+  // Only change code above this line.
+}
