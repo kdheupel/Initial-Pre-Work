6b7cd9e498fceeb0b02e4a543d868117c905bd89
kdheupel <kdheupel@gmail.com>
2019-07-11 14:04:21 -0600

    learned to pass more then one argument using hte arrow function

---
+++ 6-Introduction-to-the-ES6-Challenges/7-Write-Arrow-Functions-with-Parameters.js
@@ -0,0 +1,8 @@
+learned to pass more then one argument using hte arrow function
+
+const myConcat = (arr1, arr2) => {
+  "use strict";
+  return arr1.concat(arr2);
+};
+// test your code
+console.log(myConcat([1, 2], [3, 4, 5]));
