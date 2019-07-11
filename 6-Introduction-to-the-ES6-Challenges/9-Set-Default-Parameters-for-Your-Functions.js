20e38da5930d866a393406a5c35c079d71feaae0
kdheupel <kdheupel@gmail.com>
2019-07-11 14:20:04 -0600

    adding a default parament when an inupt is not provided - here we added the number 1 in absence of a specific input

---
+++ 6-Introduction-to-the-ES6-Challenges/9-Set-Default-Parameters-for-Your-Functions.js
@@ -0,0 +1,10 @@
+adding a default parament when an inupt is not provided - here we added the number 1 in absence of a specific input 
+
+const increment = (function() {
+  "use strict";
+  return function increment(number, value = 1) {
+    return number + value;
+  };
+})();
+console.log(increment(5, 2)); // returns 7
+console.log(increment(5)); // returns 6
