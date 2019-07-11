bc6dce188f6aa34e9b7fdc60d264b47343500fb6
kdheupel <kdheupel@gmail.com>
2019-07-11 09:21:44 -0600

    Learned how to build an object using script and array

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,19 @@
+Learned how to build an object using script and array
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  "name": "Evie",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["Chester", "Higgins"]
+
+};
