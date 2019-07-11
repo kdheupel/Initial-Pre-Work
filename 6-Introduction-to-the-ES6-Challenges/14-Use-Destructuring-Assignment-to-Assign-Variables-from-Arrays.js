ef693194c3d318518eea084d19fcd3eb1c0d2e92
kdheupel <kdheupel@gmail.com>
2019-07-11 15:12:31 -0600

    Was changing variables into an array; however, the assignment discussed using the command "const", but it did not work unless it was deleted

---
+++ 6-Introduction-to-the-ES6-Challenges/14-Use-Destructuring-Assignment-to-Assign-Variables-from-Arrays.js
@@ -0,0 +1,11 @@
+Was changing variables into an array; however, the assignment discussed using the command "const", but it did not work unless it was deleted
+
+let a = 8, b = 6;
+(() => {
+  "use strict";
+  // change code below this line
+ [a,b]=[b,a];
+  // change code above this line
+})();
+console.log(a); // should be 6
+console.log(b); // should be 8
