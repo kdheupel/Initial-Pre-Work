a67a2e73864ff16b850e4a36c7ff8454eb033d15
kdheupel <kdheupel@gmail.com>
2019-07-10 21:35:46 -0600

    applied prior lessons into a golf code Example.  being a golfer, I enjoyed this example

---
+++ 5-Introduction-to-JavaScript/72-Golf_Code.js
@@ -0,0 +1,39 @@
+applied prior lessons into a golf code Example
+
+var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+function golfScore(par, strokes) {
+  // Only change code below this line
+  if (strokes == 1){
+    return names[0];
+  }
+  else if (strokes <= par-2){
+    return names[1];
+  }
+  else if (strokes == par -1){
+    return names[2];
+  }
+  else if (strokes == par){
+    return names[3];
+  }
+  else if (strokes == par +1){
+    return names[4];
+  }
+  else if (strokes == par +2){
+    return names[5];
+  }
+  else {return names[6]}
+}
+  // Only change code above this line
+
+// Change these values to test
+golfScore(4, 1);
+golfScore(4, 2);
+golfScore(5, 2);
+golfScore(4, 3);
+golfScore(4, 4);
+golfScore(1, 1);
+golfScore(5, 5);
+golfScore(4, 5);
+golfScore(4, 6);
+golfScore(4, 7);
+golfScore(5, 9);
