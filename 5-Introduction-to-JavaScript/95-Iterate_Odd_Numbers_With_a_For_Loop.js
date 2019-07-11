cf26ed0a0dca788612955b2880ce82d8430c1d47
kdheupel <kdheupel@gmail.com>
2019-07-11 12:20:47 -0600

    creating a loop to output odd numbers; need to make sure i start on the right number; eg, 1 instead of 0

---
+++ 5-Introduction-to-JavaScript/95-Iterate_Odd_Numbers_With_a_For_Loop.js
@@ -0,0 +1,16 @@
+creating a loop to output odd numbers; need to make sure i start on the right number; eg, 1 instead of 0
+
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 10; i += 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for (var i = 1; i < 10; i += 2) {
+  myArray.push(i);
+}
