452a07ca4da73754d6154a6276814a9588fcb61e
kdheupel <kdheupel@gmail.com>
2019-07-11 13:11:36 -0600

    intordution to the parseInt function

---
+++ 5-Introduction-to-JavaScript/104-Use_the_parseInt_Function.js
@@ -0,0 +1,10 @@
+intordution to the parseInt function
+
+function convertToInteger(str) {
+ return parseInt(str)
+}
+
+convertToInteger("56");
+convertToInteger("56");
+convertToInteger("77");
+convertToInteger("JamesBond");
